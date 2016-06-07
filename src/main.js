b33f9268d1e70eded9b2e04fcb6ea1b202d1b172
foolifish07 <foolifish07@gmail.com>
2016-06-08 00:32:35 +0800

    add vue-router

--- src/main.js
+++ src/main.js
@@ -14,3 +14,39 @@ new Vue({
 		App
 	}
 })
+
+import App from './App.vue'
+
+Vue.use(VueRouter);
+
+let router = new VueRouter();
+router.beforeEach(function (transition) {
+    let to = transition.to;
+    console.log( to.path );
+    
+    if (transition.to.path === '/nothing') {
+        transition.abort();
+    } else {
+        transition.next()
+    }
+})
+
+router.map({
+
+    '/': {
+        component: App,
+        subRoutes: {
+            '/index': {
+                name: 'index',
+                component: Index,
+            },
+        }
+    }
+})
+router.redirect({
+  '/': '/index',
+  '/manager': '/manager/info',
+})
+
+let route_show = Vue.extend({});
+router.start(route_show, '#app');
