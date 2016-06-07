bb651edfe499529ae0af471d0455921de11ab5e3
foolifish07 <foolifish07@gmail.com>
2016-06-08 00:13:40 +0800

    hehe

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,5 +1,35 @@
 var config = require('./webpack.base.config.js')
 
-//config.devtool = 'eval-source-map'
+
+config.output = {
+	path: './dist',
+	filename: '[name].[hash].js',
+};
+config.devServer = {
+    hot: true,
+    inline: true,
+    progress: true,
+    historyApiFallback: true,
+    proxy: {
+    	// http://0.0.0.0:8080/Contest/715/balloon_data/
+    	'/api/*': {
+	    	target: 'http://localhost:3000',
+	        secure: false,
+	        rewrite: function(req) {
+	        	console.log('localhost:3000, 0000000000000000000000000000');
+		      	//req.url = req.url.replace( /^\/data/, 'Contest/715/balloon/data/');
+		    },
+        },
+    }
+};
+config.plugins = [
+	new HtmlWebpackPlugin({  
+	    filename: 'index.html',
+	    template: 'src/index.html',
+	    inject: 'body' 
+    })
+]
+config.devtool = 'eval-source-map'
+
 
 module.exports = config
--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,3 +1,16 @@
-var config = require('./webpack.base.config');
+var config = require('./webpack.base.config')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+config.output = {
+	path: './dist',
+	filename: '[name].js'
+},
+config.plugins = [
+	new HtmlWebpackPlugin({  
+	    filename: 'index.html',
+	    template: 'src/index.html',
+	    inject: true
+    })
+]
 
 module.exports = config
